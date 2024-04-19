a7911902f156dd54b57fcd31e6601dfd233a4bba
yokozhem <kozhemyakova.elena@gmail.com>
2024-04-19 11:38:43 -0400

    Добавлены задачи Lesson_6

---
+++ Lesson_6/task_1.py
@@ -0,0 +1,11 @@
+"""
+Вспомните какие модули вы уже проходили на курсе. 
+Создайте файл, в котором вы импортируете встроенные в модуль функции под псевдонимами. (3-7 строк импорта).
+
+"""
+import math as m
+import collections as col
+from random import randint
+import numpy as np
+import Lesson_5.task_1
+
